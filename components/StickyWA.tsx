62f4c6ad1bb3ebb33f59c5dbaba77b11f3c5ca1c
ImamMka <imammka23@gmail.com>
2026-05-07 07:15:41 +0700

    rounded added

--- components/StickyWA.tsx
+++ components/StickyWA.tsx
@@ -19,9 +19,8 @@ export function StickyWAButton() {
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Chat WhatsApp"
-      className={`fixed bottom-6 right-6 z-50 w-14 h-14 bg-green-500 hover:bg-green-400 flex items-center justify-center shadow-xl shadow-green-500/30 transition-all duration-500 hover:-translate-y-1 ${
-        show ? "opacity-100 scale-100" : "opacity-0 scale-75 pointer-events-none"
-      }`}
+      className={`fixed bottom-6 right-6 z-50 w-14 h-14 bg-green-500 hover:bg-green-400 flex items-center justify-center shadow-xl shadow-green-500/30 transition-all duration-500 hover:-translate-y-1 rounded-full  ${show ? "opacity-100 scale-100" : "opacity-0 scale-75 pointer-events-none"
+        }`}
     >
       <WhatsAppIcon className="w-7 h-7 text-white" />
     </a>
